bfa4f63c33cfef0d62d0fab4e84c1b88fbc2bd28
mr_drgh <mrdrgh2003@gmail.com>
2023-07-23 12:40:26 +0100

    mqsldkfqsdmfklq

--- _unsigned.c
+++ _unsigned.c
@@ -7,33 +7,26 @@
 int _print_unsigned(va_list var)
 {
 	unsigned int j = va_arg(var, unsigned int);
-	unsigned int number, last = j % 10, digit, expo, i;
+	unsigned int number, last = j % 10, numberr, expoon, i;
 
 	j = j / 10;
 	number = j;
-	expo = i = 1;
-	if (last < 0)
-	{
-		_putchar('-');
-		number = -number;
-		j = -j;
-		last = -last;
-		i++;
-	}
+	expoon = i = 1;
+	
 	if (number > 0)
 	{
 		while (number / 10 != 0)
 		{
-			expo = expo * 10;
+			expoon = expoon * 10;
 			number = number / 10;
 		}
 		number = j;
-		while (expo > 0)
+		while (expoon > 0)
 		{
-			digit = number / expo;
-			_putchar(digit + '0');
-			number = number - (digit * expo);
-			expo = expo / 10;
+			numberr = number / expoon;
+			_putchar(numberr + '0');
+			number = number - (numberr * expoon);
+			expoon = expoon / 10;
 			i++;
 		}
 	}
