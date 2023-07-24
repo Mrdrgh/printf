5b96ea91a988c810feac89c63e75dbb45b75a467
mr_drgh <mrdrgh2003@gmail.com>
2023-07-24 13:17:19 +0100

    pleaaase work

--- _print_Str.c
+++ _print_Str.c
@@ -6,40 +6,40 @@
 */
 int handle_S(va_list args)
 {
-char *stringg = va_arg(args, char *);
-int compteuur = 0, i;
+	char *stringg = va_arg(args, char *);
+	int compteuur = 0, i;
+	char null_stringg[] = "(null)";
 
-if (stringg == NULL)
-{
-char null_stringg[] = "(null)";
-for (i = 0; null_stringg[i] != '\0'; i++)
-{
-_putchar(null_stringg[i]);
-compteuur++;
-}
-return (compteuur);
-}
-for (i = 0; stringg[i] != '\0'; i++)
-{
-unsigned char c = stringg[i];
+	if (stringg == NULL)
+	{
+		for (i = 0; null_stringg[i] != '\0'; i++)
+		{
+			_putchar(null_stringg[i]);
+			compteuur++;
+		}
+	return (compteuur);
+	}
+	for (i = 0; stringg[i] != '\0'; i++)
+	{
+		unsigned char c = stringg[i];
 
-if (c >= 32 && c <= 127)
-{
-_putchar(c);
-compteuur++;
-}
-else
-{
-_putchar('\\');
-compteuur++;
-_putchar('x');
-compteuur++;
-_putchar("0123456789ABCDEF"[c / 16]);
-compteuur++;
-_putchar("0123456789ABCDEF"[c % 16]);
-compteuur++;
-}
-}
+		if (c >= 32 && c <= 127)
+		{
+		_putchar(c);
+		compteuur++;
+		}
+		else
+		{
+			_putchar('\\');
+			compteuur++;
+			_putchar('x');
+			compteuur++;
+			_putchar("0123456789ABCDEF"[c / 16]);
+			compteuur++;
+			_putchar("0123456789ABCDEF"[c % 16]);
+			compteuur++;
+		}
+	}
 
 return (compteuur);
 }
